7d44a7998667211abbda56658929a5d819a8a6e1
Eleven <lhqFH2818>
2017-03-20 19:49:27 +0800

    增加

--- vn.trader/ctpGateway/ctpGateway.py
+++ vn.trader/ctpGateway/ctpGateway.py
@@ -541,6 +541,8 @@ class CtpGateway(VtGateway):
     def pPosition(self,event):
         '''持仓事件处理机，当收到持仓消息时执行'''
         pos = event.dict_['data']
+        positionName = '.'.join(pos.symbol, pos.direction)
+        self.tdApi.posBufferDict[positionName] = pos
         print 'position info:'
         print pos.symbol
         print pos.exchange
