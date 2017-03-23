2b31474e6705c85bc38b8ff382eb1adb1f431af9
Eleven <lhqFH2818>
2017-03-23 22:16:03 +0800

    增加

--- vn.trader/ctpGateway/config.py
+++ vn.trader/ctpGateway/config.py
@@ -2,12 +2,12 @@
 
 class config:
     currentMode = u'多'          #当前运行模式：多或者空
-    winTarget = 5                #盈利目标点数，浮盈达到该点数，止盈
+    winTarget = 7                #盈利目标点数，浮盈达到该点数，止盈
     stopTarget = 20              #止损目标点数，浮亏达到该点数，止损
     # winTargetPrice = 100000      #止盈目标价位，当前价格达到该价格，止盈
     # stopTargetPrice = 0          #止损目标价位，当前价格达到该价格，止损
     # preSellPrice = 0             #上次平仓价位
-    maxDrawDown = 2              #允许最大回撤点数，从最高价格回撤达到该点数，止盈
+    maxDrawDown = 3              #允许最大回撤点数，从最高价格回撤达到该点数，止盈
     stopLoss = False             #是否止损
     stopWin = False              #是否止盈
 
