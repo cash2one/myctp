3244bec4ae36fb6aaf32f536b5caf8a30e83dce8
zhaoyouzhi <zhaoyouzhi@zdns.cn>
2017-04-07 12:56:35 +0800

    删除打印

--- vn.trader/ctpGateway/ctpGateway.py
+++ vn.trader/ctpGateway/ctpGateway.py
@@ -352,43 +352,43 @@ class CtpGateway(VtGateway):
                 pass
 
     def shortPolicy(self, tick):
-        # print '============================='
-        # print 'symbol:',tick.symbol
-        # print 'lastPrice:',tick.lastPrice
-        # print 'openPrice:',tick.openPrice
-        # print 'stopCount:',self.tradeDict[tick.symbol].stopCount
-        # print 'closeing:',self.tradeDict[tick.symbol].stopCount
+        print '============================='
+        print 'symbol:',tick.symbol
+        print 'lastPrice:',tick.lastPrice
+        print 'openPrice:',tick.openPrice
+        print 'stopCount:',self.tradeDict[tick.symbol].stopCount
+        print 'closeing:',self.tradeDict[tick.symbol].closeing
         if self.tradeDict[tick.symbol].stopCount >= 4:
-            # print 'step1'
+            print 'step1'
             return
         elif tick.lastPrice > tick.openPrice + self.tradeDict[tick.symbol].threshold:
             if (tick.symbol + '.3' in self.tdApi.posBufferDict.keys()) and (not self.tradeDict[tick.symbol].closeing): #存在空单
-                # print 'step3'
+                print 'step3'
                 #空单止损
                 orderReq = self.makeBuyCloseOrder(tick.symbol, tick.askPrice1,self.tdApi.posBufferDict[tick.symbol + '.3'].pos.position)
                 self.sendOrder(orderReq)
                 self.tradeDict[tick.symbol].closeing = True
                 self.tradeDict[tick.symbol].stopCount += 1
             if tick.symbol + '.2' not in self.tdApi.posBufferDict.keys():     #无持仓
-                # print 'step4'
+                print 'step4'
                 #开多单
                 self.tradeDict[tick.symbol].openFlag = True
                 self.tradeDict[tick.symbol].openDirection = u'多'
         elif tick.lastPrice < tick.openPrice - self.tradeDict[tick.symbol].threshold:
             if (tick.symbol + '.2' in self.tdApi.posBufferDict.keys()) and (not self.tradeDict[tick.symbol].closeing): #存在多单
-                # print 'step6'
+                print 'step6'
                 #多单止损
                 orderReq = self.makeSellCloseOrder(tick.symbol, tick.bidPrice1,self.tdApi.posBufferDict[tick.symbol + '.2'].pos.position)
                 self.sendOrder(orderReq)
                 self.tradeDict[tick.symbol].closeing = True
                 self.tradeDict[tick.symbol].stopCount += 1
             if tick.symbol + '.3' not in self.tdApi.posBufferDict.keys():     #无持仓
-                # print 'step7'
+                print 'step7'
                 #开空单
                 self.tradeDict[tick.symbol].openFlag = True
                 self.tradeDict[tick.symbol].openDirection = u'空'
         else:
-            # print 'step8'
+            print 'step8'
             pass
 
         # 收盘清仓
@@ -396,11 +396,13 @@ class CtpGateway(VtGateway):
         if nowTime > datetime.strptime('14:59:55', '%H:%M:%S').time() and nowTime < datetime.strptime('15:00:05', '%H:%M:%S').time():
             if tick.symbol + '.3' in self.tdApi.posBufferDict.keys(): #存在空单
                 #空单清仓
+                print 'step9'
                 orderReq = self.makeBuyCloseOrder(tick.symbol, tick.askPrice1,self.tdApi.posBufferDict[tick.symbol + '.3'].pos.position)
                 self.sendOrder(orderReq)
                 self.tradeDict[tick.symbol].closeing = True
             elif tick.symbol + '.2' in self.tdApi.posBufferDict.keys(): #存在多单
                 #多单清仓
+                print 'step10'
                 orderReq = self.makeSellCloseOrder(tick.symbol, tick.bidPrice1,self.tdApi.posBufferDict[tick.symbol + '.2'].pos.position)
                 self.sendOrder(orderReq)
                 self.tradeDict[tick.symbol].closeing = True
@@ -623,15 +625,15 @@ class CtpGateway(VtGateway):
     def pPosition(self,event):
         '''持仓事件处理机，当收到持仓消息时执行'''
         pos = event.dict_['data']
-        # for positionName in self.tdApi.posBufferDict.keys():
-        #     print '###############################'
-        #     print 'position info:'
-        #     print self.tdApi.posBufferDict[positionName].pos.symbol
-        #     print self.tdApi.posBufferDict[positionName].pos.direction
-        #     print self.tdApi.posBufferDict[positionName].pos.position
-        #     print self.tdApi.posBufferDict[positionName].pos.frozen
-        #     print self.tdApi.posBufferDict[positionName].pos.price
-        #     print self.tdApi.posBufferDict[positionName].pos.vtPositionName
+        for positionName in self.tdApi.posBufferDict.keys():
+            print '###############################'
+            print 'position info:'
+            print self.tdApi.posBufferDict[positionName].pos.symbol
+            print self.tdApi.posBufferDict[positionName].pos.direction
+            print self.tdApi.posBufferDict[positionName].pos.position
+            print self.tdApi.posBufferDict[positionName].pos.frozen
+            print self.tdApi.posBufferDict[positionName].pos.price
+            print self.tdApi.posBufferDict[positionName].pos.vtPositionName
 
     # ----------------------------------------------------------------------
     def pAccount(self, event):
