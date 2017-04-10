b8a3059b3eb4424bdc49f9f208abd6d1368f8d70
Eleven <lhqFH2818>
2017-04-10 21:23:54 +0800

    增加

--- vn.trader/ctpGateway/config.py
+++ vn.trader/ctpGateway/config.py
@@ -2,7 +2,7 @@
 
 class config:
 
-    tradeSymbol = ['RM709', 'hc1710']       #交易合约，菜粕
+    tradeSymbol = ['RM709', 'hc1710', 'bu1706']       #交易合约，菜粕
     recodeTickFlag = True        #是否记录实时行情
     CTP_configPath = '/home/myctp/vn.trader/ctpGateway/CTP_connect.json'
     TRADE_configPath = '/home/myctp/vn.trader/ctpGateway/TRADE_setting.json'
