0bf978ba61664a0ee266d5cad8fe19fd3ba27a71
zhaoyouzhi <zhaoyouzhi@zdns.cn>
2017-04-12 16:29:27 +0800

    删除打印

--- vn.trader/ctpGateway/config.py
+++ vn.trader/ctpGateway/config.py
@@ -2,7 +2,7 @@
 
 class config:
 
-    tradeSymbol = ['RM709', 'i1709', 'bu1706', 'hc1710', 'ru1709']       #交易合约，菜粕
+    tradeSymbol = ['RM709', 'i1709', 'bu1706', 'hc1710', 'ru1709', 'jm1709', 'pp1709', 'j1709', 'FG705', 'zn1705']       #交易合约，菜粕
     recodeTickFlag = True        #是否记录实时行情
     CTP_configPath = '/home/myctp/vn.trader/ctpGateway/CTP_connect.json'
     TRADE_configPath = '/home/myctp/vn.trader/ctpGateway/TRADE_setting.json'
