17ed1bca5f3fb3195039f89883eb25d9258ca9cd
Eleven <lhqFH2818>
2017-04-18 09:01:10 +0800

    增加

--- vn.trader/ctpGateway/tradeapi.py
+++ vn.trader/ctpGateway/tradeapi.py
@@ -383,8 +383,8 @@ class tradeAPI(CtpGateway):
         print 'stopCount:', self.tradeDict[tick.symbol].stopCount
         print 'closeing:', self.tradeDict[tick.symbol].closeing
         x1 = tick.openPrice + self.tradeDict[tick.symbol].tickPrice * 25
-        x2 = tick.openPrice + self.tradeDict[tick.symbol].tickPrice * 6
-        x3 = tick.openPrice - self.tradeDict[tick.symbol].tickPrice * 6
+        x2 = tick.openPrice + self.tradeDict[tick.symbol].tickPrice * 5
+        x3 = tick.openPrice - self.tradeDict[tick.symbol].tickPrice * 5
         x4 = tick.openPrice - self.tradeDict[tick.symbol].tickPrice * 25
 
         longPosition = tick.symbol + '.2'
@@ -630,13 +630,16 @@ class tradeAPI(CtpGateway):
     def pTick(self, event):
         '''tick事件处理机，当接收到行情时执行'''
         tick = event.dict_['data']
-        if self.tradeDict[tick.symbol].tickCount <= 0:
+        if self.tradeDict[tick.symbol].tickCount <= 1:
             self.tradeDict[tick.symbol].tickCount += 1
             return
         self.sendOrderMsg = True    # 只有在交易时间才允许记录成交日志和订单日志，以及发送微信消息
 
         # 获取到持仓信息后执行策略
-        self.ruPolicy(tick)
+        if tick.symbol == u'RM709':
+            self.shortPolicy4(tick)
+        else:
+            self.ruPolicy(tick)
 
         # 止损
         self.tradeStopLoss(tick)
