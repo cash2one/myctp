74e32c343358cc6a65fe5cd3b3f30311c0f96636
zhaoyouzhi <zhaoyouzhi@zdns.cn>
2017-04-27 16:45:50 +0800

    删除打印

---
+++ vn.trader/ctpGateway/mysqlApi.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+import sys
+from sqlalchemy import create_engine
+import MySQLdb
+import pandas as pd
+
+class SqlApi():
+    def __init__(self, host, user, passwd, dbname):
+        self.host = 'localhost'
+        self.user = 'root'
+        self.passwd = '880501'
+        self.dbname = 'stock'
+
+
+    def readFromSql(self, table):
+        mysql_cn = MySQLdb.connect(self.host, port=3306, user=self.user, passwd=self.passwd, db=self.dbname)
+        sql_exce = 'select count(*) from %s;' % table
+        df_mysql = pd.read_sql(sql_exce, con=mysql_cn)
+        mysql_cn.close()
+        return df_mysql
+
+    def saveToSql(self, data, table):
+        mysql_cn = MySQLdb.connect(self.host, port=3306, user=self.user, passwd=self.passwd, db=self.dbname)
+        engine = create_engine('mysql://%s:%s@%s/%s?charset=utf8' % (self.user, self.passwd, self.host, self.dbname))
+        data.to_sql(table, engine)
