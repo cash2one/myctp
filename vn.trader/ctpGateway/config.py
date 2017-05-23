b7f5a778b52421e591617e4947cb81e81c40ed4a
Eleven <lhqFH2818>
2017-05-23 12:26:46 +0800

    更改filename

--- vn.trader/ctpGateway/config.py
+++ vn.trader/ctpGateway/config.py
@@ -5,6 +5,4 @@ class config:
     tradeSymbol = ['ru1709', 'RM709', 'i1709', 'FG709', 'jd1709', 'SR709']       #交易合约，菜粕
     recodeTickFlag = True        #是否记录实时行情
     tableName = 'tradeseting'
-    CTP_configPath = '/home/myctp/vn.trader/ctpGateway/CTP_connect.json'
-    TRADE_configPath = '/home/myctp/vn.trader/ctpGateway/TRADE_setting.json'
-    BALANCE_file = '/home/myctp/vn.trader/ctpGateway/balance.csv'
+    basePath = '/home/'
