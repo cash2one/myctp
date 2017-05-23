1497c24a62c462d78aff80e1a2bfa97a2093be3b
Eleven <lhqFH2818>
2017-05-23 12:38:11 +0800

    更改filename

--- vn.trader/ctpGateway/config.py
+++ vn.trader/ctpGateway/config.py
@@ -5,4 +5,4 @@ class config:
     tradeSymbol = ['ru1709', 'RM709', 'i1709', 'FG709', 'jd1709', 'SR709']       #交易合约，菜粕
     recodeTickFlag = True        #是否记录实时行情
     tableName = 'tradeseting_doing'
-    basePath = '/home/'
+    basePath = '/work/'
