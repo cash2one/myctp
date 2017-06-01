58c8b556b2a25e61520d68817e265d4376dfc9f8
ElevenL <lhq2818@163.com>
2017-06-01 20:25:42 +0800

    增加

--- vn.trader/ctpGateway/config.py
+++ vn.trader/ctpGateway/config.py
@@ -4,8 +4,8 @@ class config:
 
     tradeSymbol = ['ru1709', 'cu1707']       #交易合约，菜粕
     recodeTickFlag = False        #是否记录实时行情
-    tableName = 'tradeseting_real'
-    basePath = '/real/'
+    tableName = 'tradeseting_gap'
+    basePath = '/gap/'
 
     # 风控
     riskControl = False      # 是否执行风控
